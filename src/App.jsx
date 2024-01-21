9521d574ab58d348f6279fb1d0e3db3175a2167f
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-21 21:49:48 +0530

    add react-router-dom and import BrowserRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <h1>Hello world!</h1>
+    </BrowserRouter>
+  );
 }
 
 export default App;
