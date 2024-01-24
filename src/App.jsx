50b3df5cca032aca552889c4c959797eee8b686a
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-24 17:22:58 +0530

    add Links for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Home from "./Home";
 import About from "./About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
