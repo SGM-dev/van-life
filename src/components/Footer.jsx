430024fa7601aaf00a6853e64b5ad876c714e388
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-26 12:39:12 +0530

    Build Footer and import to App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-black-920 absolute inset-x-0 bottom-0 px-28 py-6">
+      <p className="font-inter text-gray-350 text-center text-sm font-medium">
+        Ⓒ 2022 #VANLIFE
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
