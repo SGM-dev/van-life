1e54d2c43a465738b52b73a71952b56400789cc5
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-26 12:38:48 +0530

    use updated colors in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import Logo from "/logo.svg";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between bg-orange-50 px-6 py-9">
+    <nav className="absolute inset-x-0 top-0 flex justify-between bg-orange-50 px-6 py-9">
       <img src={Logo} className="logo max-w-36" />
-      <div className="nav-links font-inter text-gray-40 flex gap-5 text-base font-semibold">
+      <div className="nav-links font-inter text-gray-510 flex gap-5 text-base font-semibold">
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? `text-black-40 hover:text-black-40 focus:text-black-40 hover:underline focus:underline`
-              : `hover:text-black-40 focus:text-black-40 hover:underline focus:underline`
+              ? `text-black-910 hover:text-black-910 focus:text-black-910 hover:underline focus:underline`
+              : `hover:text-black-910 focus:text-black-910 hover:underline focus:underline`
           }
           to="/"
         >
@@ -19,8 +19,8 @@ function Navbar() {
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? `text-black-40 hover:text-black-40 focus:text-black-40 hover:underline focus:underline`
-              : `hover:text-black-40 focus:text-black-40 hover:underline focus:underline`
+              ? `text-black-910 hover:text-black-910 focus:text-black-910 hover:underline focus:underline`
+              : `hover:text-black-910 focus:text-black-910 hover:underline focus:underline`
           }
           to="/about"
         >
