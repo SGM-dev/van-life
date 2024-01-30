713b17bdb0d3a5288b8795e64e803e2ebc018348
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-30 23:19:14 +0530

    fix some layout issues in Vans

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -3,37 +3,44 @@ import { useEffect, useState } from "react";
 function Vans() {
   const [vans, setVans] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/vans");
+      const data = await response.json();
+      setVans(data.vans);
+    } catch (error) {
+      console.error("Error in fetching data: ", error);
+    }
+  };
+
   useEffect(() => {
-    fetch("/api/vans")
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
+    fetchData();
   }, []);
 
-  const vanElements = vans.map((van) => {
-    const vanTypeBgClass = {
-      simple: "bg-orange-610",
-      rugged: "bg-teal-800",
-      luxury: "bg-black-910 ",
-    };
+  const vanTypeBgClass = {
+    simple: "bg-orange-610",
+    rugged: "bg-teal-800",
+    luxury: "bg-black-910 ",
+  };
 
+  const vanElements = vans.map((van) => {
     return (
       <div key={van.id} className="card max-w-80 text-center">
         <img src={van.imageUrl} className="max-h-80 rounded" />
-        <div className="van-info my-2 flex justify-between gap-6 text-xl font-semibold text-black-910">
-          <div className=" text-left">
-            <h3>{van.name}</h3>
-            <p
-              className={`van-type text-orange-130 ${vanTypeBgClass[van.type]} my-1 max-w-20 flex-shrink flex-grow-0 rounded px-4 py-2 text-center text-sm font-semibold capitalize`}
-            >
-              {van.type}
+        <div className="van-info my-2 flex flex-col justify-between gap-2 text-sm font-semibold text-black-910 sm:flex-row sm:gap-6 sm:text-base">
+          <h3 className="text-left">{van.name}</h3>
+          <div className="van-price flex items-center sm:flex-col sm:text-right">
+            <h4>${van.price}</h4>
+            <p className="price-unit font-prompt text-xs font-normal sm:text-sm">
+              /day
             </p>
           </div>
-
-          <div className="van-price flex flex-col text-right">
-            <h4>${van.price}</h4>
-            <p className="price-unit font-prompt text-sm font-medium">/day</p>
-          </div>
         </div>
+        <p
+          className={`van-type text-orange-130 ${vanTypeBgClass[van.type]} my-1 max-w-20 flex-shrink flex-grow-0 rounded px-4 py-2 text-center align-middle text-xs font-semibold capitalize sm:text-sm`}
+        >
+          {van.type}
+        </p>
       </div>
     );
   });
