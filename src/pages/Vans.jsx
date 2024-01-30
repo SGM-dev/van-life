00b098f7426432356103ffc87aaf65164265dcaa
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-30 17:17:47 +0530

    map over vans and build elements

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+
 function Vans() {
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const vanElements = vans.map((van) => {
+    const vanTypeBgClass = {
+      simple: "bg-orange-610",
+      rugged: "bg-teal-800",
+      luxury: "bg-black-910 ",
+    };
+
+    return (
+      <div key={van.id} className="card max-w-80 text-center">
+        <img src={van.imageUrl} className="max-h-80 rounded" />
+        <div className="van-info my-2 flex justify-between gap-6 text-xl font-semibold text-black-910">
+          <div className=" text-left">
+            <h3>{van.name}</h3>
+            <p
+              className={`van-type text-orange-130 ${vanTypeBgClass[van.type]} my-1 max-w-20 flex-shrink flex-grow-0 rounded px-4 py-2 text-center text-sm font-semibold capitalize`}
+            >
+              {van.type}
+            </p>
+          </div>
+
+          <div className="van-price flex flex-col text-right">
+            <h4>${van.price}</h4>
+            <p className="price-unit font-prompt text-sm font-medium">/day</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <main className="flex-grow bg-orange-50 ">
-      <h1>Vans page goes here 🚙</h1>
+    <main className="flex-grow bg-orange-50 px-7 py-14 font-inter ">
+      <h1 className=" text-3xl font-bold text-black-910">
+        Explore our van options
+      </h1>
+      <section className="container mx-auto my-14 grid grid-cols-2 gap-9 md:grid-cols-3 lg:grid-cols-4">
+        {vanElements}
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,18 @@ export default {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        prompt: ["Prompt", "sans-serif"],
       },
       colors: {
+        "gray-350": "#AAA",
+        lightgray: "#D3D3D3",
         "gray-510": "#4D4D4D",
         "black-910": "#161616",
-        "gray-350": "#AAA",
         "black-920": "#252525",
-        lightgray: "#D3D3D3",
         "orange-430": "#FF8C38",
         "orange-120": "#FFCC8D",
+        "orange-130": "#FFEAD0",
+        "orange-610": "#E17654",
       },
       backgroundImage: {
         home: "linear-gradient(0deg, rgba(0, 0, 0, 0.46) 0%, rgba(0, 0, 0, 0.46) 100%), url(../src/assets/mountain.png)",
