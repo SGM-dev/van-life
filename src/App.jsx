4d4c70771a3d6fdebd0592b02e5735349ca0509b
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-02-09 09:07:45 +0530

    use relative paths for Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageLayout from "./components/PageLayout.jsx";
 import HostLayout from "./components/HostLayout.jsx";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
 import Dashboard from "./pages/Host/Dashboard.jsx";
 import Income from "./pages/Host/Income.jsx";
 import Reviews from "./pages/Host/Reviews.jsx";
-import About from "./pages/About";
-import Vans from "./pages/Vans/Vans.jsx";
 
 import "./server";
-import VanDetail from "./pages/Vans/VanDetail.jsx";
 
 function App() {
   return (
@@ -17,13 +17,15 @@ function App() {
       <Routes>
         <Route element={<PageLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
