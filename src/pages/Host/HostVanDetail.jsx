b6edc3bdfed4bae84c004a0fba7d325a56bb58c1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-02-14 21:49:06 +0530

    add container for HostVanDetail

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+import { useParams, Outlet } from "react-router-dom";
+
 function HostVanDetail() {
-  return <h1>Host Van Details</h1>;
+  const params = useParams();
+
+  const [van, setVan] = useState(null);
+
+  return (
+    <main className=" mt-4 h-5/6 justify-center rounded-md bg-white py-6 font-inter">
+      <div className="van-info">
+        {/* <img src={van.imageUrl} alt={`Picture of ${van.name} van`} /> */}
+      </div>
+    </main>
+  );
 }
 
 export default HostVanDetail;
