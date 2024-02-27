fc0895c3f5c744a2ab4bf5c4b636f898b2887037
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-02-27 23:21:43 +0530

    Add authentication requirement for host routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Vans from "./pages/Vans/Vans.jsx";
 import VanDetail from "./pages/Vans/VanDetail.jsx";
 import Login from "./pages/Login.jsx";
+import AuthRequired from "./pages/AuthRequired.jsx";
 
 import HostLayout from "./components/HostLayout.jsx";
 import Dashboard from "./pages/Host/Dashboard.jsx";
@@ -29,16 +30,17 @@ function App() {
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
           <Route path="login" element={<Login />} />
-
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
           <Route path="*" element={<NotFound />} />
