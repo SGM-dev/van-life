c36ee1ca8731fe924e477486162ac5deaf1773fa
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-02-29 16:42:26 +0530

    initiate Income component

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -1,5 +1,14 @@
 function Income() {
-  return <h1>Host Income</h1>;
+  const transactionsData = [
+    { amount: 720, date: "Jan 3, '23", id: "1" },
+    { amount: 560, date: "Dec 12, '22", id: "2" },
+    { amount: 980, date: "Dec 3, '22", id: "3" },
+  ];
+  return (
+    <main className=" justify-center py-6 font-inter">
+      <h1 className=" text-4xl font-bold text-black-910">Income</h1>
+    </main>
+  );
 }
 
 export default Income;
